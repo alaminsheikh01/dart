2feec8a74b772392084cc018bade5606548d4b6b
alamin <alamin@gmail.com>
2021-06-29 16:22:29 +0600

    first commit

---
+++ bin/dart_getup_to_speed.dart
@@ -0,0 +1,24 @@
+void main() {
+  // store value inside the myString variable
+  String myString = "Hello world!";
+
+  // print value which was inside the "myStiring" variable
+  print(myString);
+
+  // check if myString variable containe the value of "Hello"
+  // then returen the boolean value "True" or "false"
+  print(myString.contains('Hello'));
+
+  int myIneteger = 6;
+  print(myIneteger.isOdd);
+  double myDoubel = 5.6;
+
+  num myNumber = 6.6;
+  print(myNumber);
+
+  bool myBol = false;
+
+  dynamic mySomething = 5;
+  mySomething = 'Hello';
+  mySomething = true;
+}
