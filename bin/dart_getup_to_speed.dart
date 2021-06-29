3c5bf76eaa51d51534afdc980b3954c192b39bbe
alamin <alamin@gmail.com>
2021-06-29 17:30:22 +0600

    non nullability

--- bin/dart_getup_to_speed.dart
+++ bin/dart_getup_to_speed.dart
@@ -1,28 +1,5 @@
 void main() {
-  // store value inside the myString variable
-  String myString = "Hello world!";
-
-  // print value which was inside the "myStiring" variable
-  print(myString);
-
-  // check if myString variable containe the value of "Hello"
-  // then returen the boolean value "True" or "false"
-  print(myString.contains('Hello'));
-
-  int myIneteger = 6;
-  print(myIneteger.isOdd);
-  double myDoubel = 5.6;
-
-  num myNumber = 6.6;
-  print(myNumber);
-
-  bool myBol = false;
-
-  dynamic mySomething = 5;
-  mySomething = 'Hello';
-  mySomething = true;
-  final test =
-      'Hello final'; // you will can't change or override value with test variable. test variable used only once.
-  print(test);
-  final String demo = "Hello";
+  // String? impossible = null;
+  String? possible = 'abc';
+  print(possible.length);
 }
