24d026221be6bc353ad0e9c34f5c171e4dfdad12
alamin <alamin@gmail.com>
2021-06-29 18:32:07 +0600

    play with function and function parameter

--- bin/dart_getup_to_speed.dart
+++ bin/dart_getup_to_speed.dart
@@ -15,3 +15,21 @@ void otherFunction() {
   returnString();
   // returnStringNested(); // we cann't have this function out of box.
 }
+
+void positionalParams(int x, double y, String greeting) {
+  positionalParams(5, 3.5, "Hello");
+}
+
+void optionalParams(int x, double y, [String? greeting]) {
+  optionalParams(5, 3.5);
+  optionalParams(5, 3.5, "Hi");
+}
+
+void namedOptionalParams({int? x, double? y, String? greeding}) {
+  namedOptionalParams(x: 5, y: 3.4, greeding: 'hi');
+}
+
+void namedrequiredParams(
+    {required int? x, required double? y, required String? greeding}) {
+  namedrequiredParams(x: 5, y: 3.2, greeding: "Heli");
+}
