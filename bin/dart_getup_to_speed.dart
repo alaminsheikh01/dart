9dbfd516e62c161ec26c584ee801980d687ae5df
alamin <alamin@gmail.com>
2021-06-29 17:44:08 +0600

    dart operation

--- bin/dart_getup_to_speed.dart
+++ bin/dart_getup_to_speed.dart
@@ -1,5 +1,18 @@
 void main() {
-  // String? impossible = null;
-  String? possible = 'abc';
-  print(possible.length);
+  int result;
+  double resultDouble = 5 / 5; // output should be double
+  result = 5 ~/ 5; // output should be integer
+
+  int x = 5;
+  x++; // 6
+  x--; // 5
+  x = x + 5; // 10
+
+  bool isBool = 5 == 5;
+  print(isBool); // will return "true" or "false"
+
+  //String myString = "Hello " + isBool.toString(); // normal method
+  //String myString = 'Hello $isBool'; // this is called string interpolution
+  String myString = 'Hello ${10 + 20}'; // this is called string interpolution
+  print(myString);
 }
