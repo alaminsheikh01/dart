6ddb35bf305406bac7896b517e3e953ad63f2fe7
alamin <alamin@gmail.com>
2021-07-05 16:00:26 +0600

    enum

--- bin/dart_getup_to_speed.dart
+++ bin/dart_getup_to_speed.dart
@@ -1,19 +1,7 @@
+enum AccountType { free, premium, vip }
+
 void main() {
-  final names = ['alamin', 'sheikh', 'tanvir'];
-
-  final nameFiltered = names.where((name) => name.length == 4).toList();
-
-  for (int i = 0; i < nameFiltered.length; i++) {
-    print(nameFiltered[i]);
-  }
-
-  for (final name in nameFiltered) {
-    print(name);
-  }
-
-  final x = [
-    for (int i = 0; i < 5; i++) i.toString(),
-  ];
-
-  print(x);
+  AccountType userAccountType = AccountType.premium;
+  print(userAccountType.index);
+  AccountType.values[1];
 }
