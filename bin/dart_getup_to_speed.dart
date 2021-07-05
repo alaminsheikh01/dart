d375b10457daa6ffd0b3d14c17b2a79355b2b3d7
alamin <alamin@gmail.com>
2021-07-05 15:07:12 +0600

    using loop by different way

--- bin/dart_getup_to_speed.dart
+++ bin/dart_getup_to_speed.dart
@@ -1,35 +1,19 @@
 void main() {
-  String returnStringNested() {
-    return 'hello inside';
+  final names = ['alamin', 'sheikh', 'tanvir'];
+
+  final nameFiltered = names.where((name) => name.length == 4).toList();
+
+  for (int i = 0; i < nameFiltered.length; i++) {
+    print(nameFiltered[i]);
   }
 
-  print(returnStringNested());
-  print(returnString());
-}
+  for (final name in nameFiltered) {
+    print(name);
+  }
 
-String returnString() {
-  return 'hello outside';
-}
+  final x = [
+    for (int i = 0; i < 5; i++) i.toString(),
+  ];
 
-void otherFunction() {
-  returnString();
-  // returnStringNested(); // we cann't have this function out of box.
-}
-
-void positionalParams(int x, double y, String greeting) {
-  positionalParams(5, 3.5, "Hello");
-}
-
-void optionalParams(int x, double y, [String? greeting]) {
-  optionalParams(5, 3.5);
-  optionalParams(5, 3.5, "Hi");
-}
-
-void namedOptionalParams({int? x, double? y, String? greeding}) {
-  namedOptionalParams(x: 5, y: 3.4, greeding: 'hi');
-}
-
-void namedrequiredParams(
-    {required int? x, required double? y, required String? greeding}) {
-  namedrequiredParams(x: 5, y: 3.2, greeding: "Heli");
+  print(x);
 }
